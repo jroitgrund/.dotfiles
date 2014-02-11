7c192a425d2ff0966a6d5fa2242b031d31f16c31
jonathan <jonathan@jonathan-pc.Home>
2014-02-11 04:17:22 +0000

    Initial commit

---
+++ xmonad.hs
@@ -0,0 +1,18 @@
+import XMonad
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Cursor
+import XMonad.Util.EZConfig
+
+main = xmonad defaults
+
+defaults = ewmh defaultConfig {
+        terminal           = "rxvt",
+        borderWidth        = 0,
+        modMask            = mod4Mask,
+        layoutHook         = avoidStruts $ layoutHook defaultConfig,
+        manageHook         = manageDocks <+> manageHook defaultConfig,
+        startupHook        = setDefaultCursor xC_left_ptr
+    } `additionalKeys`
+    [ ((mod4Mask, xK_p), spawn "dmenu_launch")
+    ]
